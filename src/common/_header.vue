2e45a1d51c5f3043e5529555a615bc95518474b2
zhouchdi <15728042326@163.com>
2018-03-20 15:49:25 +0800

    2018-3-20 建立文件格式

---
+++ src/common/_header.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "_header"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+//引入element-ti
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +16,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
