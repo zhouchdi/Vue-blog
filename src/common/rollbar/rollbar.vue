2b632be0197c6ff83dd73b7d93e012c03f0cb0e9
zhouchdi <15728042326@163.com>
2018-04-11 17:26:45 +0800

    2018-4-11:17:30 rollbar

---
+++ src/common/rollbar/rollbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="rollbar">
+      <div>
+          <i class=""></i>
+      </div>
+      <h4>去顶部</h4>
+  </div>
+</template>
+<script>
+    export default{
+        name:'rollbar'
+    }
+</script>
+<style lang='less'>
+
+</style>
